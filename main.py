dc2243af825445f568d9cffd9efd234139c83161
SQZ0111 <saqibbhatti.public@gmail.com>
2026-06-16 11:47:48 +0200

    feat:  main update

--- main.py
+++ main.py
@@ -1,58 +1,27 @@
-#While Schleife
-
-
-# x = 0
-# while x < 10:
-#     print("Hallo Welt")
-#     if x == 5:
+user_tries = 0
+# while True:
+#     if user_tries == 3:
+#         print("End")
 #         break
-#     x+= 1  
+#     user_name = input("Input username\n")
+#     if user_name == "admin":
+#         print("welcome!")
+#         break
+#     user_tries += 1 #user_tries = user_tries + 1
+
+for i in range(0,3):
+    user_name = input("Input username\n")
+    if user_name == "admin":
+        print("welcome!")
+        break
+
+print("End")
+
+# for i in range(0,4):
+#    print(i)
+
+# for i in "text":
+#     print(i)
 
 
-import datetime
-import random
-#login 
-
-
-#1. Usereingaben in variablen speichern (username,password)
-input_try = 0
-while input_try < 3:  
-    username = input("Gebe den Usernamen ein\n").lower()
-    password = input("Gebe das Passwort ein\n").lower()
-    input_try += 1
-    if input_try == 3:
-        print("Drei fehlerhafte Eingaben, beende Programm!")
-    if username == "root" and password == "admin":
-
-        print(f"""
-            ********Willkommen {username}********
-            #1. Zeige Systemwerte
-            #2. Zeige Uhrzeit an t
-            #3. Lasse eine Zufallszahl generieren
-            #4. Exit
-            """)
-        user_option = input("Wähle eine entsprechende Option aus! [1|2|3|4]")
-        if user_option == "1":
-            print("""
-                ABC: ...
-                ABC:...
-                
-                """)
-            break
-        elif user_option == "2":
-            now = datetime.datetime.now()
-            print(str(now.time()))
-            break
-        elif user_option == "3":
-            pseudo_random_number = random.randrange(1,10)
-            print(pseudo_random_number)
-            break
-        else:
-            print("CYA!")
-            break
-    else:
-        print("Falsche Eingabe, Bitte Username und/oder Password richtig eingeben!")
-    
-
-print("Programm beendet!")
-    
+#1. Ersetze alle while schleifen durch for Schleifen (nach Möglichkeit)
